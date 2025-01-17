23c4a4ef283a0cf88c21cd0610dce31ac65b9518
HarishWiz <harishharish8122@gmail.com>
2025-01-17 17:29:36 +0000

    Updated the project with new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import AmazonLogin from './Components/LoginPages/AmazonLogin'
-import YahooSignin from './Components/LoginPages/YahooSignin'
+
 import Main from './Components/Main'
-import ErrorPage from './Components/ErrorPage';
+
 
 function App() {
 
   return (
     <>
-   <BrowserRouter>
-   <Routes>
-    <Route path='/' element={<Main/>}/>
-    <Route path='amazon' element={<AmazonLogin/>}/>
-    <Route path='/yahoo' element={<YahooSignin/>}/>
-    <Route path='*' element={<ErrorPage/>}/>
-   </Routes>
-   </BrowserRouter>
+      <Main />
     </>
   )
 }
